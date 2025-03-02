641ae984774b3ea5bed9f519830b85a4c58bb8d8
Vaibhav <vaibhavanandsingh24@gmail.com>
2025-03-02 21:27:11 +0530

    environment variables setup for deployment

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import fs from 'fs';
+import 'dotenv/config';
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT;
 
 app.use(cors());
 app.use(bodyParser.json());
--- src/store/domainsSlice.ts
+++ src/store/domainsSlice.ts
@@ -1,8 +1,8 @@
 import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3001/api';
-
+const API_URL = import.meta.env.VITE_API_URL;
+// const API_URL = "http://localhost:3001/api";
 // Types
 export interface Option {
   id: number;
